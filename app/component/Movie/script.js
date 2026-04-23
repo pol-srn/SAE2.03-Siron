fbca46bdbb9b9c67cf4863b645f90a8fcfcfb0c5
Siron Paul <siron2@mmi-dev.unilim.fr>
2026-04-23 13:41:29 +0200

    update credentials

---
+++ app/component/Movie/script.js
@@ -0,0 +1,30 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+let templateLiFile = await fetch("./component/Movie/templateLi.html");
+let templateLi = await templateLiFile.text();
+
+let Movie = {};
+
+Movie.format = function (data) {
+    let html = template;
+    if (data.length == 0) {
+        return html.replaceAll(
+            "{{list}}",
+            "<p class='movie__empty'>Aucun film n'est disponible pour le moment</p>",
+        );
+    } else {
+
+        let htmlList = "";
+        for (let list of data) {
+            let li = templateLi;
+            li = li.replaceAll("{{image}}", "../server/images/" +  list.image);
+            li = li.replaceAll("{{title}}", list.name);
+    
+            htmlList += li;
+        }
+        html = html.replaceAll("{{list}}", htmlList);
+        return html;
+    }
+};
+
+export { Movie };
--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -1,5 +1,5 @@
 // URL où se trouve le répertoire "server" sur mmi.unilim.fr
-let HOST_URL = "..";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
+let HOST_URL = "https://mmi.unilim.fr/~siron2/SAE2.03-Siron";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
 
 let DataMovie = {};
 
@@ -7,7 +7,7 @@ DataMovie.requestMovies = async function(){
     // fetch permet d'envoyer une requête HTTP à l'URL spécifiée. 
     // L'URL est construite en concaténant HOST_URL à "/server/script.php?direction=" et la valeur de la variable dir. 
     // L'URL finale dépend de la valeur de HOST_URL et de dir.
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies)");
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
     // answer est la réponse du serveur à la requête fetch.
     // On utilise ensuite la méthode json() pour extraire de cette réponse les données au format JSON.
     // Ces données (data) sont automatiquement converties en objet JavaScript.
