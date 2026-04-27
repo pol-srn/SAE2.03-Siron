82fee4a4ada60c252ceb98df571827e4d19cc721
Siron Paul <siron2@mmi-dev.unilim.fr>
2026-04-27 08:43:42 +0200

    update credentials

--- admin/component/MovieForm/script.js
+++ admin/component/MovieForm/script.js
@@ -1,15 +1,24 @@
 let templateFile = await fetch('./component/MovieForm/template.html');
 let template = await templateFile.text();
+let templateLiFile = await fetch("./component/MovieForm/templateLi.html");
+let templateLi = await templateLiFile.text();
 
 
 let MovieForm = {};
 
-MovieForm.format = function(handler){
-    let html= template;
-    html = html.replace('{{handler}}', handler);
+MovieForm.format = function (handler) {
+    let html = template;
+    let listMovie = "";
+    for (let movie of data) {
+        let li = templateLi;
+        li = li.replaceAll("{{id}}", movie.id);
+
+        listMovie += li;
+    }
+    html = html.replace('{{handler}}', listMovie);
     return html;
 }
 
 
-export {MovieForm};
+export { MovieForm };
 
